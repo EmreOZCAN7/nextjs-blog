5768b7b570bfbb64a8d1a5732a0366a94e6c7747
Emre Özcan <ynsmrzcn7@gmail.com>
2022-09-23 14:11:23 +0300

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,11 @@
 export const getGlobalData = () => {
-  const name = process.env.BLOG_NAME
-    ? decodeURI(process.env.BLOG_NAME)
-    : 'Emre Özcan';
+  const name = 'Emre Özcan';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Under Construction';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : '2022 - Emre Özcan';
 
   return {
     name,
