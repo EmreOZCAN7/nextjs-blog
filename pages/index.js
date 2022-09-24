8d028b4a4c17eb3f2deca50fbcc829a7d35bca94
Emre Özcan <107696151+EmreOZCAN7@users.noreply.github.com>
2022-09-24 15:45:22 +0300

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -15,32 +15,23 @@ export default function Index({ posts, globalData }) {
       <Header name={globalData.name} />
       <main className="w-full">
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
-         Under Construction 
+          Full Stack Developer
         </h1>
         <ul className="w-full">
-          {posts.map((post) => (
-            <li
-              key={post.filePath}
-              className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
-            >
+          <li
+            className="md:first:rounded-t-lg md:last:rounded-b-lg backdrop-blur-lg bg-white dark:bg-black dark:bg-opacity-30 bg-opacity-10 hover:bg-opacity-20 dark:hover:bg-opacity-50 transition border border-gray-800 dark:border-white border-opacity-10 dark:border-opacity-10 border-b-0 last:border-b hover:border-b hovered-sibling:border-t-0"
+          >
+            <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
+              <p className="uppercase mb-3 font-bold opacity-60">
+              </p>
+              <h2 className="text-2xl md:text-3xl">Follow me on;</h2>
               
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
-                  {post.data.date && (
-                    <p className="uppercase mb-3 font-bold opacity-60">
-                      
-                    </p>
-                  )}
-                  <h2 className="text-2xl md:text-3xl">Follow me on;</h2>
-                  
-                    <p className="mt-3 text-lg opacity-60">
-                
-                    </p>
-                  
-                
-                </a>
-            
-            </li>
-          ))}
+                <div onClick={()=>null}>
+                <p className="mt-3 text-lg opacity-60">Github</p>
+                </div>
+              
+            </a>
+          </li>
         </ul>
       </main>
       <Footer copyrightText={globalData.footerText} />
