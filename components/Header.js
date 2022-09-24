d5501e26b5a3dcc2989974caa353cc85c1f8252d
Emre Özcan <ynsmrzcn7@gmail.com>
2022-09-24 17:04:02 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -2,8 +2,8 @@ import Link from 'next/link';
 
 export default function Header({ name }) {
   return (
-    <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+    <header className="pt-14 pb-10">
+      <img src='https://avatars.githubusercontent.com/u/107696151?v=4' className="w-32 h-32 rounded-full block mx-auto mb-1 bg-gradient-conic from-gradient-3 to-gradient-4" />
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
